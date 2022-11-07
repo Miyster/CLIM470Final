5e8b6fb11a256386966ae8e89a1501c79dcff2d1
tvv18 <93282632+tvv18@users.noreply.github.com>
2022-11-07 10:08:45 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,4 +1,5 @@
 # Use matplotlib and cartopy to make plots for wind vectors, see paper for plot ideas (taylor will look into this once code is written and able to be exported)
-# taylor idea dump:
+# taylor idea/research dump:
   # https://scitools.org.uk/iris/docs/v2.2/examples/Meteorology/wind_speed.html
+  # https://matplotlib.org/3.1.1/api/_as_gen/matplotlib.pyplot.quiver.html
   # find way to create wind barbs
