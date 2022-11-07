7658f9f8d1467ff4caf227e72f35bed9253d35a1
tvv18 <93282632+tvv18@users.noreply.github.com>
2022-11-07 09:59:26 -0500

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,1 +1,1 @@
-# Use matplotlib and cartopy to make plots for wind vectors, see paper for plot ideas
+# Use matplotlib and cartopy to make plots for wind vectors, see paper for plot ideas (taylor will look into this once code is written and able to be exported)
