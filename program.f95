ddaf18e5bcd42a1f20f3f789ecc6a296069c983c
Laurenwest13 <116679069+Laurenwest13@users.noreply.github.com>
2022-12-09 23:47:24 -0500

    Update program.f95

--- program.f95
+++ program.f95
@@ -14,7 +14,7 @@ INTEGER :: d,Lx,Ly,Nx,Ny,h0,nstep
 REAL :: hs_t, delt, f_1, f_2, f_3
 !REAL, pointer :: alp0(:,:,:),bet0(:,:,:),gam0(:,:,:),del(:,:,:),eps0(:,:,:),ken0(:,:,:),phi0(:,:,:),q0(:,:,:),z0(:,:,:),hu0(:,:,:),hv0(:,:,:),hq0(:,:,:),us0(:,:,:),vs0(:,:,:)
 !REAL, allocatable :: u(:,:),v(:,:),h(:,:),z(:,:),q(:,:),hs(:,:),phi(:,:),ken(:,:), hs(:,:)
-REAL, DIMENSION(:,:,:), ALLOCATABLE :: alp0,bet0,gam0,del0,eps0,ken0,phi0,q0,z0,hu0,hu1,hu2,hu3,hv0,hq0,us0,vs0, ght0
+REAL, DIMENSION(:,:,:), ALLOCATABLE :: alp0,bet0,gam0,del0,eps0,ken0,phi0,q0,z0,hu0,hu1,hu2,hu3,hv0,hv1,hv2,hv3,hq0,us0,vs0, ght0
 REAL, DIMENSION(:,:), ALLOCATABLE :: u, v, h, z, q, hs, phi, ken, ght
 REAL, parameter:: f_cor=10e-04, g=9.8
 !something is off about these, it keeps saying variables are already assigned when they are not ("Symbol 'h' at (1) already has basic type of REAL)
@@ -72,11 +72,17 @@ SELECT CASE (d)
 !it into one comma separated command
 
 allocate(hu0(Nx,Ny,3)) 
-allocate(hv0(Nx,Ny,3))
-allocate(hq0(Nx,Ny,3))
 allocate(hu1(Nx,Ny,3)) 
 allocate(hu2(Nx,Ny,3)) 
-allocate(hu3(Nx,Ny,3)) 
+allocate(hu3(Nx,Ny,3))
+
+allocate(hq0(Nx,Ny,3))
+
+allocate(hv0(Nx,Ny,3))
+allocate(hv1(Nx,Ny,3))
+allocate(hv2(Nx,Ny,3))
+allocate(hv3(Nx,Ny,3))
+
 allocate(us0(Nx,Ny,3))
 allocate(vs0(Nx,Ny,3)) 
 allocate(alp0(Nx,Ny,3)) 
@@ -289,13 +295,13 @@ do i = 2, Nx-1
 end do
 
 do j = 2, Ny-1
-        hv(:,j) = (h(:,j-1) + h(:,j+1))/2.0
+        hv0(:,j) = (h(:,j-1) + h(:,j+1))/2.0
 end do
     
 
 
     us(:,:) = hu0(:,:)*u(:,:)
-    vs(:,:) = hv(:,:)*v(:,:)
+    vs(:,:) = hv0(:,:)*v(:,:)
 
     us1 = us2
     us2 = us3
@@ -308,7 +314,7 @@ end do
     hu3 = hu0
     hv1 = hv2 
     hv2 = hv3
-    hv3 = hv
+    hv3 = hv0
 
     if (nstep == 1440) then
         write(10,*) h, u, V
