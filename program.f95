5aeb9639c028a2c2070ca1fdf45d256dafe12785
HurricaneBrian <116753190+HurricaneBrian@users.noreply.github.com>
2022-12-11 19:57:26 -0500

    Update program.f95
    Added equation 3.15 from Arakawa for the definition of hq0 for a square grid

--- program.f95
+++ program.f95
@@ -317,7 +317,7 @@ do n = 2,3
 end do
 
 ! what is hq0??????? add in parentheses
-!hq0(:,:,n) = ()
+!hq0(:,:,n) = (h(i+1/2,j+1/2)+h(i-1/2,j+1/2)+h(i-1/2,j-1/2)+h(i+1/2,j-1/2))/4            !Added eqn 3.15 from Arakawa for the def of hq0 for a square grid 
 q0(:,:,n) = (f_cor+z0(:,:,n))/hq0(:,:,n) 
 
 do i = 1,Nx
